ea36a9a3695db79d6708fc4418e8f2c641bdd5f6
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-05-05 11:09:55 +0200

    customizing database schema

--- store/models.py
+++ store/models.py
@@ -37,6 +37,11 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateTimeField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    class Meta:
+        db_table = "store.customers"
+        indexes = [
+            models.Index(fields=['last_name', 'first_name'])
+        ]
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
