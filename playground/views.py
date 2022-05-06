4b009aef492f2261237961a241cd4a5473a91976
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-05-06 10:16:36 +0200

    selecting fields to query

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.db.models import F
-from store.models import Product
+from store.models import Product, OrderItem
 
 # Create your views here.
 def say_hello(request):
-    # limit result to 5 early object
-    query_set = Product.objects.all()[:5]
+    # # value return dictionary, value_list return tupple
+    # query_set = Product.objects.values('id', 'title', 'collection__id')
+
+    # select product that have been ordered and sort them by their title
+    query_set = Product.objects.filter(
+        pk__in=OrderItem.objects.values('product_id').distinct()
+        ).order_by('title')
 
     return render(request, "hello.html", {'name': 'Matahari Ramadhan', 'products': list(query_set)})
