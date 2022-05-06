7f53104f058970cc603141fc2627d6f225fd77f0
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-05-06 09:06:23 +0200

    referencing fields using F objects

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
-from django.db.models import Q
+from django.db.models import F
 from store.models import Product
 
 # Create your views here.
 def say_hello(request):
-    # products: inventory < 10 AND inventory < 20, this simple query can be done like this:
-    # query_set = Product.objects.filter(inventory__lt=10, inventory__lt=20)
-
-    # products: inventory < 10 AND NOT inventory < 20, this complex query can be done like this:
-    query_set = Product.objects.filter(Q(inventory__lt=10) & ~Q(inventory__lt=20))
-
+    # products: inventory = collection__id
+    query_set = Product.objects.filter(inventory=F('collection_id'))
 
     return render(request, "hello.html", {'name': 'Matahari Ramadhan', 'products': list(query_set)})
