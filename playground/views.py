289e60a1d13bc7d4970a91bd0e0457bc0cae919d
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-05-06 09:52:02 +0200

    sorting

--- playground/views.py
+++ playground/views.py
@@ -4,7 +4,11 @@ from store.models import Product
 
 # Create your views here.
 def say_hello(request):
-    # products: inventory = collection__id
-    query_set = Product.objects.filter(inventory=F('collection_id'))
+    # # sorting by unit_price dsc AND title asc
+    query_set = Product.objects.order_by('unit_price', '-title').reverse()
+
+    # get first object
+    # product = Product.objects.order_by('unit_price')[0]
+    # product = Product.objects.earliest('unit_price')
 
     return render(request, "hello.html", {'name': 'Matahari Ramadhan', 'products': list(query_set)})
