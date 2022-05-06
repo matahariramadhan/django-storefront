67b8817e4f3b4f958f6811521f9d2c6099878562
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-05-06 09:55:38 +0200

    limiting result

--- playground/views.py
+++ playground/views.py
@@ -4,11 +4,7 @@ from store.models import Product
 
 # Create your views here.
 def say_hello(request):
-    # # sorting by unit_price dsc AND title asc
-    query_set = Product.objects.order_by('unit_price', '-title').reverse()
-
-    # get first object
-    # product = Product.objects.order_by('unit_price')[0]
-    # product = Product.objects.earliest('unit_price')
+    # limit result to 5 early object
+    query_set = Product.objects.all()[:5]
 
     return render(request, "hello.html", {'name': 'Matahari Ramadhan', 'products': list(query_set)})
