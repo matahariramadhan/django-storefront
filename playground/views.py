81c4a42e995ed9f0c5da09f8448f41aab7e29b21
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-05-06 10:40:55 +0200

    deferring fields

--- playground/views.py
+++ playground/views.py
@@ -4,12 +4,9 @@ from store.models import Product, OrderItem
 
 # Create your views here.
 def say_hello(request):
-    # # value return dictionary, value_list return tupple
-    # query_set = Product.objects.values('id', 'title', 'collection__id')
-
-    # select product that have been ordered and sort them by their title
-    query_set = Product.objects.filter(
-        pk__in=OrderItem.objects.values('product_id').distinct()
-        ).order_by('title')
+    # becareful using only and defer can cause performance issue
+    # below is example of performance issue
+    # query_set = Product.objects.only('title')
+    query_set = Product.objects.defer('unit_price')
 
     return render(request, "hello.html", {'name': 'Matahari Ramadhan', 'products': list(query_set)})
