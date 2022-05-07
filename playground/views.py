916b08a1059eb0ea50cc0951b821954801bbeef5
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-05-07 08:17:52 +0200

    calling database function

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,19 @@
+from operator import concat
 from unittest import result
 from django.shortcuts import render
-from django.db.models import Value, F
+from django.db.models import Value, F, Func
+from django.db.models.functions import Concat
 from store.models import Product, OrderItem, Order, Customer
 
 # Create your views here.
 def say_hello(request):
-    queryset = Customer.objects.annotate(new_id=F('id')+1)
+    # queryset = Customer.objects.annotate(
+    #     # using funtion object
+    #     full_name=Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT')
+    # )
+
+    queryset = Customer.objects.annotate(
+        # using Concat from django
+        full_name=Concat('first_name', Value(' '), 'last_name')
+    )
     return render(request, "hello.html", {'name': 'Matahari Ramadhan', 'result': queryset})
