18f77c55213b769f17b998596a50ce6afff06b48
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-05-07 07:54:17 +0200

    aggregating objects

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
+from django.db.models.aggregates import Count, Min, Max
 from store.models import Product, OrderItem, Order
 
 # Create your views here.
 def say_hello(request):
-    # select_related for one
-    # query_set = Product.objects.select_related('collection').all()
+    result = Product.objects.filter(collection_id=1).aggregate(count=Count('id'), min_price=Min('unit_price'))
 
-    # pre_fetch for many
-    # query_set = Product.objects.prefetch_related('promotion').all()
-
-    # get the last 5 orders with their customer and items (including product)
-    query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('placed_at')[:5]
-
-    return render(request, "hello.html", {'name': 'Matahari Ramadhan', 'orders': list(query_set)})
+    return render(request, "hello.html", {'name': 'Matahari Ramadhan', 'result': result})
